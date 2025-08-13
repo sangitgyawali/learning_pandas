4ec987224fab124e6752592827a29d89e12cc11b
Sangit Gyawali <developersangeet@gmail.com>
2025-08-13 09:31:29 +0545

    Adding columns in pandas

---
+++ adding.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Name": ["Ram", "Shyam", "Ghanshyam", "Dhanshyam", "Aditi", "Jagdish", "Raj", "Simran"],
+    "Age": [28, 34, 22, 30, 29, 40, 25, 32],
+    "Salary": [50000, 60000, 45000, 52000, 49000, 70000, 48000, 58000],
+    "Performance_Score": [85, 90, 78, 92, 88, 95, 80, 89]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+df["Bonus"] = df['Salary'] * 0.1
+print(df)
