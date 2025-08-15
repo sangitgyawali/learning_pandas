54f99c96a078dfa59878562be7be33e73bf1e2f2
Sangit Gyawali <developersangeet@gmail.com>
2025-08-15 22:03:58 +0545

    Concatenation of dataframes

---
+++ Merging & Joining/concatenation.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df_Region1 = pd.DataFrame({
+    'CustomerID':[1, 2],
+    'Name':['Gopal', 'Raju']
+})
+
+df_Region2 = pd.DataFrame({
+    'CustomerID':[3, 4],
+    'Name':['Shyam', 'Baburam']
+})
+
+df_concat = pd.concat([df_Region1, df_Region2], axis=1, ignore_index=True)
+print(df_concat)
