fd44bf65fcd468a3ffeb36c4df4ed60ad9906c21
Sangit Gyawali <developersangeet@gmail.com>
2025-08-15 19:56:33 +0545

    Merging data using inner join

---
+++ Merging & Joining/merging.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df_customers = pd.DataFrame({
+    'CustomerID': [1, 2, 3],
+    'Name': ['Ramesh', 'Suresh', 'Kalpesh']
+})
+
+df_orders = pd.DataFrame({
+    'CustomerID': [1, 2, 4],
+    'OrderAmount': [250, 450, 350]
+})
+
+df_merged = pd.merge(df_customers, df_orders, on="CustomerID", how="inner")
+print("Inner join")
+print(df_merged)
